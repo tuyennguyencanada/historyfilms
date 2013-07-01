b2ee4f6bbacc4abf29cbdb140e71b2ff202b2b2c
tuyennguyencanada <tuyennguyencanada@gmail.com>
2013-06-30 23:11:22 -0600

    Initial Commit

---
+++ HistoryFilms/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HistoryFilms
+//
+//  Created by Tuyen Nguyen on 6/30/2013.
+//  Copyright (c) 2013 PropertySpots Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HistoryFilmsTests/HistoryFilmsTests.m
@@ -0,0 +1,36 @@
+//
+//  HistoryFilmsTests.m
+//  HistoryFilmsTests
+//
+//  Created by Tuyen Nguyen on 6/30/2013.
+//  Copyright (c) 2013 PropertySpots Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HistoryFilmsTests : XCTestCase
+
+@end
+
+@implementation HistoryFilmsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
